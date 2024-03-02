b2d0d32a3fa3800640089608bbedf3df98392c5f
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-03-02 13:15:29 +0530

    Edited package.json, singleBlog app

--- src/Pages/SingleBlog.jsx
+++ src/Pages/SingleBlog.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
-import { FaClock, FaUser } from 'react-icons/fa';
+
 import SideBar from '../Components/SideBar';
 import { useLoaderData } from 'react-router-dom';
 

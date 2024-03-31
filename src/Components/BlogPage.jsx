5ea7c274386ccc622141f76a3025600e95e7135a
Anukeerth-ek <152538146+Anukeerth-ek@users.noreply.github.com>
2024-03-31 18:18:01 +0530

    Edited BlogsPage.jsx

--- src/Components/BlogPage.jsx
+++ src/Components/BlogPage.jsx
@@ -19,7 +19,8 @@ const BlogPage = ({ type, color }) => {
           const fetchBlogs = async () => {
                setLoading(true);
                try {
-                    const apiUrl = "../dist/modern-react-js-blog-starter-files/api/blogsData.json"; 
+                    const apiUrl = "/modern-react-js-blog-starter-files/api/blogsData.json";
+
                     let url = `${apiUrl}?page=${currentPage}&limit=${pageSize}`;
                     if (selectedCategory) {
                          url += `&category=${selectedCategory}`;
